e9388b6887d426b7c7da3dc20896daf4a22811d0
Adam Cmiel <adamcmiel@gmail.com>
2013-12-08 12:17:49 -0600

    attempt _.delay

--- src/underbar.js
+++ src/underbar.js
@@ -285,6 +285,7 @@ var _ = { };
   // parameter. For example _.delay(someFunction, 500, 'a', 'b') will
   // call someFunction('a', 'b') after 500ms
   _.delay = function(func, wait) {
+    setTimeout(func.apply(this, [].slice.apply(arguments).slice(2)), wait);
   };
 
 
